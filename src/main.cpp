a119caebfb992a822c693ac89f20e25fc6e9cf70
Mango T14 <kubajaskow@wp.pl>
2024-01-23 23:57:09 +0100

    Add main

--- src/main.cpp
+++ src/main.cpp
@@ -1,77 +1,60 @@
-#include <SFML/Graphics.hpp>
-#include "Solver.hpp"
+#include <iostream>
 
-auto test(sf::RenderTarget& target) -> void;
+#include "engine/window_context_handler.hpp"
+#include "engine/common/color_utils.hpp"
 
-int main() {
-    constexpr int32_t window_width  = 1000;
-    constexpr int32_t window_height = 1000;
+#include "physics/physics.hpp"
+#include "thread_pool/thread_pool.hpp"
+#include "renderer/renderer.hpp"
 
-    sf::ContextSettings settings;
-    settings.antialiasingLevel = 1;
-    sf::RenderWindow window(sf::VideoMode(window_width, window_height), "Verlet", sf::Style::Default, settings);
-    const uint32_t frame_rate = 60;
-    window.setFramerateLimit(frame_rate);
 
-    Solver   solver;
+int main()
+{
+    const uint32_t window_width  = 1920;
+    const uint32_t window_height = 1080;
+    WindowContextHandler app("Verlet-MultiThread", sf::Vector2u(window_width, window_height), sf::Style::Default);
+    RenderContext& render_context = app.getRenderContext();
+    // Initialize solver and renderer
 
-    // Solver configuration
-    solver.setConstraint({static_cast<float>(window_width) * 0.5f, static_cast<float>(window_height) * 0.5f}, 450.0f);
-    solver.setSubStepsCount(8);
-    solver.setSimulationUpdateRate(frame_rate);
+    tp::ThreadPool thread_pool(10);
+    const IVec2 world_size{300, 300};
+    PhysicSolver solver{world_size, thread_pool};
+    Renderer renderer(solver, thread_pool);
 
-    // Set simulation attributes
-    const float        object_spawn_delay    = 0.5f;
-    const float        object_spawn_speed    = 600.0f;
-    const sf::Vector2f object_spawn_position = {500.0f, 500.0f};
-    const uint32_t     object_count          = 5;
-    
-    sf::Clock clock;
-    while (window.isOpen()) {
-        for (auto event = sf::Event{}; window.pollEvent(event);) {
-            if (event.type == sf::Event::Closed) {
-                window.close();
-                
+    const float margin = 20.0f;
+    const auto  zoom   = static_cast<float>(window_height - margin) / static_cast<float>(world_size.y);
+    render_context.setZoom(zoom);
+    render_context.setFocus({world_size.x * 0.5f, world_size.y * 0.5f});
+
+    bool emit = true;
+    app.getEventManager().addKeyPressedCallback(sf::Keyboard::Space, [&](sfev::CstEv) {
+        emit = !emit;
+    });
+
+    constexpr uint32_t fps_cap = 60;
+    int32_t target_fps = fps_cap;
+    app.getEventManager().addKeyPressedCallback(sf::Keyboard::S, [&](sfev::CstEv) {
+        target_fps = target_fps ? 0 : fps_cap;
+        app.setFramerateLimit(target_fps);
+    });
+
+    // Main loop
+    const float dt = 1.0f / static_cast<float>(fps_cap);
+    while (app.run()) {
+        if (solver.objects.size() < 80000 && emit) {
+            for (uint32_t i{20}; i--;) {
+                const auto id = solver.createObject({2.0f, 10.0f + 1.1f * i});
+                solver.objects[id].last_position.x -= 0.2f;
+                solver.objects[id].color = ColorUtils::getRainbow(id * 0.0001f);
             }
         }
 
-        if (solver.getObjectsCount() < object_count && clock.getElapsedTime().asSeconds() >= object_spawn_delay) {
-            clock.restart();
-            auto&   object = solver.addObject(object_spawn_position, 25.0);
-            solver.setObjectVelocity(object, object_spawn_speed * sf::Vector2f{1.0, 0.0});
-        }
+        solver.update(dt);
 
-        solver.update();
-        window.clear(sf::Color::White);
-        const sf::Vector3f constraint = solver.getConstraint();
-        sf::CircleShape constraint_background{constraint.z};
-        constraint_background.setOrigin(constraint.z, constraint.z);
-        constraint_background.setFillColor(sf::Color::Black);
-        constraint_background.setPosition(constraint.x, constraint.y);
-        constraint_background.setPointCount(128);
-        window.draw(constraint_background);
-
-        // Render objects
-        sf::CircleShape circle{1.0f};
-        circle.setPointCount(32);
-        circle.setOrigin(1.0f, 1.0f);
-        const auto& objects = solver.getObjects();
-        for (const auto& obj : objects) {
-            circle.setPosition(obj.position);
-            circle.setScale(obj.radius, obj.radius);
-            circle.setFillColor(obj.fill_color);
-            window.draw(circle);
-        }
-		window.display();
+        render_context.clear();
+        renderer.render(render_context);
+        render_context.display();
     }
-}
 
-auto test(sf::RenderTarget& target) -> void {
-    sf::Vector2f v = {960.0f, 540.0f};
-    sf::CircleShape circle(100.0f, 100);
-    circle.setOrigin(50.0f, 50.0f);
-    circle.setPosition(v);
-    circle.setScale(1.0f, 1.0f);
-    circle.setFillColor(sf::Color::Magenta);
-    target.draw(circle);
+    return 0;
 }
